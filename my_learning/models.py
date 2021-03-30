e35605884df1ae2c8181b1fee95ac1246e528fba
Pritam Kumar <Pritam8987@gmail.com>
2021-03-30 12:38:38 -0400

    added admin model

--- my_learning/models.py
+++ my_learning/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """A Topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
