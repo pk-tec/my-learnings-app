71fd484e10b30a661468e1e40bdb4e0e02b02958
Pritam Kumar <Pritam8987@gmail.com>
2021-03-31 15:01:37 -0400

    added new entry page

--- my_learning/models.py
+++ my_learning/models.py
@@ -4,6 +4,11 @@ class Topic(models.Model):
     """A Topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
 
 class Entry(models.Model):
     """Something specific learned about a topic."""
--- my_learning/urls.py
+++ my_learning/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     
     # Page for adding a new topic
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     
 ]
