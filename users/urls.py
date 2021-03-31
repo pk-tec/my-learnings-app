c49de0cf574077f0ca45a57615ee87c596da9d9b
Pritam Kumar <Pritam8987@gmail.com>
2021-03-31 19:35:03 -0400

    added authentication and authorization

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 """Defines URL patterns for users"""
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Includes default auth urls. 
     path('', include('django.contrib.auth.urls')),
+    
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
